cf2a4a99e4ebb26fd038bf28182738a178584075
kousalya <kousalyahkspt7@gmail.com>
2025-04-08 15:24:36 +0530

    Add files via upload

---
+++ bank management system/arith_demo.java
@@ -0,0 +1,20 @@
+//arithmetic
+class arith_demo
+{
+	public static void main(String args[])
+	{
+		int x=10,y=20;
+		int sum;
+		int sub;
+		int div;
+		int mul;
+		sum=x+y;
+		sub=x-y;
+		div=x/y;
+		mul=x*y;
+		System.out.println("add of 2 numbers="+sum);
+		System.out.println("sub of 2 numbers="+sub);
+		System.out.println("div of 2 numbers="+div);
+		System.out.println("mul of 2 numbers="+mul);
+	}
+}
